7ffb3afb2b6a8ac4875a9808a3fd4c3cc9b91a76
Allan_Felipe <allan.borges23@outlook.com>
2024-09-03 09:59:39 -0300

    Product, Category created

---
+++ src/main/java/com/alandev/dscommerceproject/entities/Category.java
@@ -0,0 +1,48 @@
+package com.alandev.dscommerceproject.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<Product>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+}

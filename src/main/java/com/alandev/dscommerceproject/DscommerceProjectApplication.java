e6b1b9a02b5e1e721fe95414ef02f3956f1fa1a5
Allan_Felipe <allan.borges23@outlook.com>
2024-09-03 09:06:38 -0300

    Initial Commit

---
+++ src/main/java/com/alandev/dscommerceproject/DscommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alandev.dscommerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DscommerceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alandev/dscommerceproject/DscommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alandev.dscommerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

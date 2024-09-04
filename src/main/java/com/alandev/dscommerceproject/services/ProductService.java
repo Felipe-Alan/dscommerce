97086327d6718711c01c8443b87f4dca2076fa24
Allan_Felipe <allan.borges23@outlook.com>
2024-09-04 13:43:05 -0300

    PUT product

--- src/main/java/com/alandev/dscommerceproject/services/ProductService.java
+++ src/main/java/com/alandev/dscommerceproject/services/ProductService.java
@@ -3,6 +3,7 @@ package com.alandev.dscommerceproject.services;
 import com.alandev.dscommerceproject.dto.ProductDTO;
 import com.alandev.dscommerceproject.entities.Product;
 import com.alandev.dscommerceproject.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,5 +29,27 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        BeanUtils.copyProperties(dto, entity, "id");
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        Product entity = repository.getReferenceById(id);
+        BeanUtils.copyProperties(dto, entity, "id");
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
     }
 }

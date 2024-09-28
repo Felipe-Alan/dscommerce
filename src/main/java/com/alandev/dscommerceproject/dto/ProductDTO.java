f01bdf1de330c485a4def2b05cfc649b7dfe262d
Allan_Felipe <allan.borges23@outlook.com>
2024-09-28 08:07:18 -0300

    Fix validation

--- src/main/java/com/alandev/dscommerceproject/dto/ProductDTO.java
+++ src/main/java/com/alandev/dscommerceproject/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.alandev.dscommerceproject.dto;
 
 import com.alandev.dscommerceproject.entities.Category;
 import com.alandev.dscommerceproject.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private String name;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
 
